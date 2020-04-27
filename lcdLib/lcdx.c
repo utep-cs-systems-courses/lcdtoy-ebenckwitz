056f9aece1699b9543cf695d2767fa9b1c0540d1
Ethan <ebenckwitz>
2020-04-26 18:57:11 -0600

    custom shape with specific position

--- lcdLib/lcdx.c
+++ lcdLib/lcdx.c
@@ -14,47 +14,25 @@ main()
   
   clearScreen(COLOR_BLUE);
 
-  // draw_triangle(10, 50);
-  // draw_triangle2(10,30,50);
-  my_shape(40, 80);
+  my_shape(50, 85, 30);
 
 }
- 
-void draw_triangle(int size, int place)
+
+void my_shape(u_char centerC, u_char centerR, u_char size)
 {
-  //triangle
-  u_char row = size;
-  u_char lol = 0;
-  for(u_char col = 0; col < size; col++, row++){
-      drawPixel(col + place, col + place, COLOR_WHITE);
-      drawPixel(-col + place, col + place, COLOR_BLACK);
-      drawPixel(lol + size, place + size, COLOR_RED);
-      drawPixel(row + size, place + size, COLOR_ORANGE);
-      lol++;
-  }
-}
-void draw_triangle2(u_char fc, u_char fr, u_char size)
-{
-  for(u_char c = 0; c < size; c++){
-    u_char topRow = 0;
-    u_char botRow = size - c;
-    for(u_char r = topRow; r < botRow; r++)
-      drawPixel(fc, fr, COLOR_RED);
-  }
-}
-void my_shape(u_char TR, u_char BR)
-{
-  u_char size = BR - TR;
-  u_char topRow = TR;
-  u_char botRow = BR;
-  for(int c = 0; c < size; c++, TR++, BR++) {
-    drawPixel(TR, topRow, COLOR_WHITE);
-    drawPixel(TR, botRow, COLOR_GREEN);
-    for(u_char k = topRow; k < botRow; k++)
-      drawPixel(topRow, k, COLOR_BLACK);
+  u_char TR = centerR - (size / 2);
+  u_char BR = centerR + (size / 2);
+  u_char SC = centerC - 15;
+  u_char edge = SC;
+  
+  for(int c = 0; c < size; c++, SC++) {
+    drawPixel(SC, TR, COLOR_WHITE);
+    drawPixel(SC, BR, COLOR_GREEN);
+    for(u_char k = TR; k < BR; k++)
+      drawPixel(edge, k, COLOR_BLACK);
   }
   for(int c = 0; c < size / 2; c++) {
-    drawPixel(TR, topRow++, COLOR_RED);
-    drawPixel(TR++, botRow--, COLOR_BLACK);
+    drawPixel(SC, TR++, COLOR_RED);
+    drawPixel(SC++, BR--, COLOR_BLACK);
   }
 }
