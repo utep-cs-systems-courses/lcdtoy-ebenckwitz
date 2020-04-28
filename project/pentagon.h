7b31eff2978b0fe5d24f770822757a37955e55cc
Ethan <ebenckwitz>
2020-04-27 20:16:03 -0600

    filled my shape with color

---
+++ project/pentagon.h
@@ -0,0 +1,12 @@
+void pentagon(u_char centerC, u_char centerR, u_char size);
+
+typedef struct Ab_pentagon_s {
+  void (*getBounds)(const struct Ab_pentagon_s *pentagon, const Vec2 *centerPos, Region *bounds);
+  int (*check)(const struct Ab_pentagon_s *shape, const Vec2 *centerPos, const Vec2 *pixel);
+  const Vec2 halfSize;
+
+} Ab_pentagon;
+
+void ab_pentagonGetBounds(const Ab_pentagon *pentagon, const Vec2 *centerPos, Region *bounds);
+int ab_pentagonCheck(const Ab_pentagon *pentagon, const Vec2 *centerPos, const Vec2 *pixel);
+
