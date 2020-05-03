c7d5490bca47d215308ae0d9b619b44588e11eb9
Ethan <ebenckwitz>
2020-05-03 17:35:00 -0600

    fixed 8x12 font

--- project/8x12font.c
+++ project/8x12font.c
@@ -5,21 +5,20 @@
 void drawChar8x12(u_char rcol, u_char rrow, char c, u_int fgColorBGR, u_int bgColorBGR)
 {
   u_char oc = c - 0x20;
-  u_char bit = 0x01;
-  u_char col = 0;
-  u_char row = 0;
-
-  lcd_setArea(rcol, rrow, rcol + 7, rrow + 12); /* relative to requested col/row */
-  while (row < 12) {
-    while (col < 8) {
-      u_int colorBGR = (font_8x12[oc][col] & bit) ? fgColorBGR : bgColorBGR;
-      lcd_writeColor(colorBGR);
-      col++;
-    }
-  col = 0;
-  bit <<= 1;
-  row++;
-  }
+   u_char bit = 128;
+   u_char col = 0;
+   u_char row = 0;
+  //9,12
+  lcd_setArea(rcol, rrow, rcol + 7, rrow + 11); /* relative to requested col/row */
+  while (col < 10) {
+     while (bit > 0) {
+       u_int colorBGR = (font_8x12[oc][col] & bit) ? fgColorBGR : bgColorBGR;
+       lcd_writeColor(colorBGR);
+       bit >>= 1;
+     }
+     col++;
+     bit = 128;
+  } 
 }
 
 void drawString8x12(u_char col, u_char row, char *string, u_int fgColorBGR, u_int bgColorBGR)
