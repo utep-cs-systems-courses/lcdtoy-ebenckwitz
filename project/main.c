727d9e51f53b832d1b834f53f1fd0797d1441c60
Ethan <ebenckwitz>
2020-05-03 17:56:05 -0600

    updated

--- project/main.c
+++ project/main.c
@@ -13,17 +13,7 @@ int main(void)
   switch_init();
   lcd_init();
   led_init();
-  //welcome();
   clearScreen(COLOR_GREEN);
-  //  drawString5x7(20,20, "hello", COLOR_BLACK, COLOR_GREEN);
-  // state_advance();
+  drawString8x12(20,20, "hello", COLOR_BLACK, COLOR_GREEN);
    or_sr(0x18); //CPU off, GIE on
-}
-
-void welcome()
-{
-  red_on = 0;
-  green_on = 1;
-  led_update();
-  clearScreen(COLOR_RED);
 }
--- project/stateMachines.c
+++ project/stateMachines.c
@@ -14,7 +14,6 @@ void state_advance()
   switch(current_state) {
   case 1:
     state_one();
-    //drawString5x7(20,20, "hello", COLOR_BLUE, COLOR_GREEN);
     break;
  
   case 2:
@@ -25,7 +24,8 @@ void state_advance()
 
 void state_one()
 {
-  clearScreen(COLOR_YELLOW);
+  clearScreen(COLOR_GREEN);
+  drawString8x12(20, 20, "hello", COLOR_BLACK, COLOR_GREEN);
   pentagon(50, 80, 30);
   red_on = 1;
   green_on = 1;
@@ -35,7 +35,7 @@ void state_one()
 void state_two()
 {
   clearScreen(COLOR_GREEN);
-  drawString8x12(20, 20, "cool", COLOR_BLUE, COLOR_GREEN);
+  drawString8x12(20, 20, "hello", COLOR_BLACK, COLOR_GREEN);
   red_on = 0;
   green_on = 1;
   led_update();
